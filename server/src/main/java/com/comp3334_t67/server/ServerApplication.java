6a2f3dd188f87ac8875ca58eebcefc4c7db515f1
SoniaL25 <sonia.lim.2023@scis.smu.edu.sg>
2026-03-28 10:58:38 +0800

    add server

---
+++ server/src/main/java/com/comp3334_t67/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package com.comp3334_t67.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
